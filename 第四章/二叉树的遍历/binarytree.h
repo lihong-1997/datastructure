28947d0134fc9f2e13d1adb2ea164befc3a31eed
ci <2353557436@qq.com>
2023-03-25 17:25:24 +0800

    先序和中序序列确定二叉树、判断两棵树是否同构、求节点左右儿子

--- 第四章/二叉树的遍历/binarytree.h
+++ 第四章/二叉树的遍历/binarytree.h
@@ -194,5 +194,59 @@ int Height(TreeNode<T>* root)
         return 0;
 }
 
+//先序和中序遍历序列确定二叉树
+template <class T>
+TreeNode<T>* BuildTree(std::vector<T>& pre, std::vector<T>& in)
+{
+    //确保遍历序列非空，且作为递归基准情形
+    if (pre.empty() || in.empty()) return nullptr;
+
+    //先序序列第一个值为根节点值，创建根节点
+    T rootVal = pre[0];
+    TreeNode<T>* root = new TreeNode<T>(rootVal);
+
+    //由根节点值，在中序序列中获得左子树右子树节点个数
+	int index = 0; 
+    for (;index < in.size();index++)
+        if (in[index] == rootVal) break;
+    //构造前序序列中的左右子树
+    std::vector<T> leftChildTree_pre(pre.begin() + 1, pre.begin() + 1 + index);
+    std::vector<T> rightChildTree_pre(pre.begin() + 1 + index, pre.end());
+    //构造中序序列中的左右子树
+    std::vector<T> leftChildTree_in(in.begin(), in.begin() + index);
+    std::vector<T> rightChildTree_in(in.begin() + index + 1, in.end());
+
+	//前序的左子树序列与中序的左子树序列
+    root->left = BuildTree(leftChildTree_pre, leftChildTree_in);
+    //前序的右子树序列与中序的右子树序列
+    root->right = BuildTree(rightChildTree_pre, rightChildTree_in);
+
+    return root;
+}
+
+//先序遍历递归版求节点左右儿子
+template <class T>
+void PreOrderGetChild(TreeNode<T>* root)
+{
+    if (root)
+    {
+        T lChild = (root->left) ? (root->left->val) : '-';
+        T rChild = (root->right) ? (root->right->val) : '-';
+        std::cout << root->val << ":" << lChild << rChild << '\n';
+
+        PreOrderGetChild(root->left);
+        PreOrderGetChild(root->right);
+    }
+}
+
+//判断两棵树是否同构
+template <class T>
+bool isIsomorphic(TreeNode<T>* root1, TreeNode<T>* root2) {
+    if (!root1 && !root2) return true; //两节点都没子树
+    if (!root1 || !root2) return false; //两节点其中一个没子树
+    if (root1->val != root2->val) return false; //两节点都有子树，判断子树根节点值
+    return (isIsomorphic(root1->left, root2->left) && isIsomorphic(root1->right, root2->right)) ||
+        (isIsomorphic(root1->left, root2->right) && isIsomorphic(root1->right, root2->left));
+}
 
 #endif
--- 第四章/二叉树的遍历/traverse.cpp
+++ 第四章/二叉树的遍历/traverse.cpp
@@ -2,44 +2,93 @@
 
 int main()
 {
-    TreeNode<char>* root = new TreeNode<char>('A');
-    root->left = new TreeNode<char>('B');
-    root->left->left = new TreeNode<char>('D');
-    root->left->right = new TreeNode<char>('F');
-    root->left->right->left = new TreeNode<char>('E');
+    //TreeNode<char>* root = new TreeNode<char>('A');
+    //root->left = new TreeNode<char>('B');
+    //root->left->left = new TreeNode<char>('D');
+    //root->left->right = new TreeNode<char>('F');
+    //root->left->right->left = new TreeNode<char>('E');
 
-    root->right = new TreeNode<char>('C');
-    root->right->left = new TreeNode<char>('G');
-    root->right->right = new TreeNode<char>('I');
-    root->right->left->right = new TreeNode<char>('H');
+    //root->right = new TreeNode<char>('C');
+    //root->right->left = new TreeNode<char>('G');
+    //root->right->right = new TreeNode<char>('I');
+    //root->right->left->right = new TreeNode<char>('H');
 
-    std::cout << "先序遍历迭代：";
-    PreOrderTraversal(root);
-    std::cout << "先序遍历递归：";
-    PreOrderTraversal_Recursion(root);
+    //std::cout << "先序遍历迭代：";
+    //PreOrderTraversal(root);
+    //std::cout << "先序遍历递归：";
+    //PreOrderTraversal_Recursion(root);
+    //std::cout << std::endl;
+
+    //std::cout << "中序遍历迭代：";
+    //InOrderTraversal(root);
+    //std::cout << "中序遍历递归：";
+    //InOrderTraversal_Recursion(root);
+    //std::cout << std::endl;
+
+    //std::cout << "后序遍历迭代：";
+    //PostOrderTraversal(root);
+    //std::cout << "后序遍历递归：";
+    //PostOrderTraversal_Recursion(root);
+
+    //std::cout << std::endl;
+    //std::cout << "层序遍历迭代：";
+    //LevelOrderTraversal(root);
+
+    //std::cout << std::endl;
+    //std::cout << "二叉树叶子";
+    //PreOrderPrintLeaves(root);
+
+    //std::cout << std::endl;
+    //std::cout << "二叉树高度:" << Height(root);
+
+    //std::cout << std::endl;
+    //std::vector<char> PreOrder = { 'a','b','c','d','e','f','g','h','i','j' };
+    //std::vector<char> InOrder = { 'c','b','e','d','a','h','g','i','j','f' };
+
+    //TreeNode<char>* root0 = BuildTree(PreOrder, InOrder);
+    //LevelOrderTraversal(root0); //a b f c d g e h i j
+
+    //std::cout << std::endl;
+
+    //判断树是否同构
+    TreeNode<char>* root1 = CreateNode('A');
+	root1->left = CreateNode('B');
+    root1->left->left = CreateNode('D');
+    root1->left->right = CreateNode('E');
+    root1->left->right->left = CreateNode('F');
+    root1->right = CreateNode('C');
+    root1->right->left = CreateNode('G');
+    root1->right->left->left = CreateNode('H');
+
+    TreeNode<char>* root2 = CreateNode('A');
+    root2->left = CreateNode('C');
+    root2->left->left = CreateNode('G');
+    root2->left->left->right = CreateNode('H');
+    root2->right = CreateNode('B');
+    root2->right->left = CreateNode('E');
+    root2->right->right = CreateNode('D');
+    root2->right->left->left = CreateNode('F');
+
+    TreeNode<char>* root3 = CreateNode('A');
+    root3->left = CreateNode('B');
+    root3->left->left = CreateNode('G');
+    root3->left->left->right = CreateNode('H');
+    root3->right = CreateNode('C');
+    root3->right->right = CreateNode('E');
+    root3->right->left = CreateNode('D');
+    root3->right->left->left = CreateNode('F');
+
+    LevelOrderTraversal(root1);
+    std::cout << std::endl;
+    LevelOrderTraversal(root2);
     std::cout << std::endl;
 
-    std::cout << "中序遍历迭代：";
-    InOrderTraversal(root);
-    std::cout << "中序遍历递归：";
-    InOrderTraversal_Recursion(root);
-    std::cout << std::endl;
+    bool isIso1 = isIsomorphic(root1, root2);
+    bool isIso2 = isIsomorphic(root1, root3);
+    std::cout << "tree1 tree2 is isomorphic? " << isIso1 << "\n"
+              << "tree1 tree3 is isomorphic? " << isIso2 << std::endl;
 
-    std::cout << "后序遍历迭代：";
-    PostOrderTraversal(root);
-    std::cout << "后序遍历递归：";
-    PostOrderTraversal_Recursion(root);
-
-    std::cout << std::endl;
-    std::cout << "层序遍历迭代：";
-    LevelOrderTraversal(root);
-
-    std::cout << std::endl;
-    std::cout << "二叉树叶子";
-    PreOrderPrintLeaves(root);
-
-    std::cout << std::endl;
-    std::cout << "二叉树高度:" << Height(root);
+    PreOrderGetChild(root1);
 
     return 0;
 }
